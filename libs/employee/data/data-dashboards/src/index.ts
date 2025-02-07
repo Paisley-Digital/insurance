40e0d9cced0fe24f4d3bb4fe7a6c5bf3eb1242b9
H.ShariatMovahed <hamidshariatwebdeveloper@gmail.com>
2025-02-07 12:34:24 +0330

    refactor and create mock data

---
+++ libs/employee/data/data-dashboards/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/dashboard.model';
+export * from './lib/dashboard.mock';
--- libs/employee/feature/dashboard/src/lib/employee-feature-dashboard.component.ts
+++ libs/employee/feature/dashboard/src/lib/employee-feature-dashboard.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { dashboardMock } from '@insurance-employee-data-dashboards';
+import { MatCard, MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'insurance-dashboard',
-  imports: [CommonModule,MatButtonModule,MatIconModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCard,
+    MatCardContent,
+  ],
   templateUrl: './employee-feature-dashboard.component.html',
   styleUrl: './employee-feature-dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  protected readonly dashboardMock = dashboardMock;
+}

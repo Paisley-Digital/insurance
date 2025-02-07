9f166087b19d39211a611933776d3023239a6680
Aria <ariaman2002@gmail.com>
2025-02-07 16:35:30 +0330

    Handle upload file for Emirates id.

--- libs/employee/feature/upload/src/lib/upload/employee-feature-upload.component.ts
+++ libs/employee/feature/upload/src/lib/upload/employee-feature-upload.component.ts
@@ -29,10 +29,12 @@ import { MatCardModule } from '@angular/material/card';
 export class EmployeeFeatureUploadComponent {
   filePreview: string | ArrayBuffer | null = null;
   passportFilePreview: string | ArrayBuffer | null = null;
+  filePreviewEmiratesId: string | ArrayBuffer | null = null;
   isImage: boolean = false;
   fileType: 'image' | 'video' = 'image';
   fileSize = signal('');
   fileSizePassport = signal('');
+  fileSizeId = signal('');
 
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
@@ -74,10 +76,6 @@ export class EmployeeFeatureUploadComponent {
     return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
   }
 
-  removeFile() {
-    this.filePreview = null;
-  }
-
   onFileSelectedPassport(event: Event) {
     const input = event.target as HTMLInputElement;
 
@@ -87,12 +85,10 @@ export class EmployeeFeatureUploadComponent {
 
     const file = input.files[0];
     if (!file) {
-      console.warn('No file selected.');
       return;
     }
 
     if (file.size > 2 * 1024 * 1024) {
-      alert('File size exceeds 2MB!');
       return;
     }
 
@@ -112,7 +108,48 @@ export class EmployeeFeatureUploadComponent {
     }
   }
 
+  onFileSelectedId(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files) {
+      return;
+    }
+
+    const file = input.files[0];
+    if (!file) {
+      return;
+    }
+
+    if (file.size > 2 * 1024 * 1024) {
+      alert('File size exceeds 2MB!');
+      return;
+    }
+
+    const fileType = file.type;
+    this.isImage = fileType.startsWith('image/');
+    this.fileSizeId.set(this.formatFileSize(file.size));
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.filePreviewEmiratesId = reader.result;
+    };
+
+    if (this.isImage) {
+      reader.readAsDataURL(file);
+    } else {
+      this.filePreviewEmiratesId = null;
+    }
+  }
+
+  removeFile() {
+    this.filePreview = null;
+  }
+
   removeFilePassport() {
     this.passportFilePreview = null;
+  }
+
+  removeFileId() {
+    this.filePreviewEmiratesId = null;
   }
 }

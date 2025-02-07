63d36a0b80b74d37ab0e7aae62ad85e18abdb230
Aria <ariaman2002@gmail.com>
2025-02-07 17:53:57 +0330

    Set user name password for login.

--- libs/employee/feature/auth/src/lib/employee-feature-auth.component.ts
+++ libs/employee/feature/auth/src/lib/employee-feature-auth.component.ts
@@ -1,15 +1,29 @@
-import {Component, inject, signal} from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatIcon, MatIconModule} from "@angular/material/icon";
-import {MatFormField, MatSuffix} from "@angular/material/form-field";
-import {MatDrawerContainer, MatSidenavModule} from "@angular/material/sidenav";
-import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatCheckbox, MatCheckboxModule} from "@angular/material/checkbox";
-import {MatAnchor, MatButton, MatButtonModule, MatIconButton} from "@angular/material/button";
-import {MatInput, MatInputModule} from "@angular/material/input";
-import {ErrorMessageComponent} from "@shared-ui-input-validator";
-import {Router} from "@angular/router";
-import {isHandsetScreen} from "@shared-util-common";
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatFormField, MatSuffix } from '@angular/material/form-field';
+import {
+  MatDrawerContainer,
+  MatSidenavModule,
+} from '@angular/material/sidenav';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
+import {
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatIconButton,
+} from '@angular/material/button';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { ErrorMessageComponent } from '@shared-ui-input-validator';
+import { Router } from '@angular/router';
+import { isHandsetScreen } from '@shared-util-common';
+import { OverlaySpinnerDirective } from '@insurance-shared-ui-overlay-spinner';
 
 @Component({
   selector: 'insurance-employee-feature-auth',
@@ -25,6 +39,7 @@ import {isHandsetScreen} from "@shared-util-common";
     MatButtonModule,
     ErrorMessageComponent,
     MatCheckboxModule,
+    OverlaySpinnerDirective,
   ],
   templateUrl: './employee-feature-auth.component.html',
   styleUrl: './employee-feature-auth.component.scss',
@@ -36,20 +51,32 @@ export class EmployeeFeatureAuthComponent {
   isHandsetScreen$ = isHandsetScreen();
 
   showPassword = signal(false);
+  loading = signal(false);
 
   loginForm = this.formBuilder.group({
     userName: ['', Validators.required],
     password: ['', Validators.required],
   });
 
+  get errorMessage() {
+    if (this.loginForm.get('password')?.hasError('notValid')) {
+      return 'The username or password is incorrect.';
+    }
+    return '';
+  }
+
   isShowPassword() {
     this.showPassword.update((current) => !current);
   }
 
   navigateToProduct() {
-    if (this.loginForm.invalid) {
+    const userName = this.loginForm.getRawValue().userName;
+    const password = this.loginForm.getRawValue().password;
+    if (userName !== 'bbgroup' || password !== '123') {
+      this.loginForm.get('password')?.setErrors({ notValid: true });
       return;
     }
+    this.loading.set(true);
     this.router.navigate(['/console']);
   }
 }

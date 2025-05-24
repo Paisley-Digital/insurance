f34e34aaae4b8f7895aea6e95ac52e06a470a13b
Aria <ariaman2002@gmail.com>
2025-05-24 16:45:53 +0330

    Add mat-stepper in ekyc management

--- libs/broker/feature/ekyc-management/src/lib/broker-feature-ekyc-management.component.ts
+++ libs/broker/feature/ekyc-management/src/lib/broker-feature-ekyc-management.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { MatCard } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { AlertService } from '@shared-ui-alert';
 
 @Component({
   selector: 'insurance-broker-feature-ekyc-management',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCard,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIcon,
+    NgOptimizedImage,
+  ],
   templateUrl: './broker-feature-ekyc-management.component.html',
   styleUrl: './broker-feature-ekyc-management.component.scss',
 })
-export class BrokerFeatureEkycManagementComponent {}
+export class BrokerFeatureEkycManagementComponent {
+  private _formBuilder = inject(FormBuilder);
+  private alert = inject(AlertService);
+
+  filePreview: string | ArrayBuffer | null = null;
+  passportFilePreview: string | ArrayBuffer | null = null;
+  visaFilePreview: string | ArrayBuffer | null = null;
+  filePreviewEmiratesId: string | ArrayBuffer | null = null;
+  file = signal<File | null>(null);
+  fileSize = signal('');
+
+  companyInfoForm = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files) return;
+    const file = input.files[0];
+    if (file && file.size <= 2 * 1024 * 1024) {
+      this.file.set(file);
+      this.fileSize.set(this.formatFileSize(file.size));
+      this.updateFilePreview(file, 'residency');
+    } else {
+      this.alert.open('File size exceeds 2MB!');
+    }
+  }
+
+  removeFile() {
+    this.filePreview = null;
+    this.file.set(null);
+  }
+
+  updateFilePreview(file: File, type: string) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (type === 'residency') {
+        this.filePreview = reader.result;
+      } else if (type === 'passport') {
+        this.passportFilePreview = reader.result;
+      } else if (type === 'emiratesId') {
+        this.filePreviewEmiratesId = reader.result;
+      }
+    };
+    reader.readAsDataURL(file);
+  }
+
+  private formatFileSize(bytes: number): string {
+    if (bytes < 1024) return `${bytes} B`;
+    if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(2)} KB`;
+    return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+  }
+}
